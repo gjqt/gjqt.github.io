f9325e8c25bc4b9ab159d259b15176bd5e9a59ad
gjqt <642941824@qq.com>
2014-04-10 11:06:05 +0800

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -19,6 +19,7 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
 // Restart the game
 GameManager.prototype.restart = function () {
   maxscore = 2;
+  localStorage.maxscore = 2;
   this.storageManager.clearGameState();
   this.actuator.continueGame(); // Clear the game won/lost message
   this.setup(); 
