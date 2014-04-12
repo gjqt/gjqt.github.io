5d969fb58606dc413a99b2fd9e09a21f547cabfc
bjdc <997285765@qq.com>
2014-04-12 09:00:05 +0800

    bug

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -169,7 +169,7 @@ HTMLActuator.prototype.message = function (won) {
   
   var text3 = function (m) { var r = 0; while (m > 1) r++, m >>= 1; return r; }
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "恭喜你打败了欧阳少恭！" : mytxt[text3(maxscore)-3];
+  var message = won ? "恭喜你打败了欧阳少恭！" : mytxt[text3(maxValue)-3];
 
   if (typeof ga !== "undefined") {
     ga("send", "event", "game", "end", type, this.score);
